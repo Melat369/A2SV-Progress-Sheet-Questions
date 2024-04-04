5ecf6911fb49d8daaf1163e762b3c81adb6af58d
Melat t <melattesfaye16@gmail.com>
2024-04-04 19:25:35 +0300

    969. Pancake Sorting

---
+++ 969. Pancake Sorting.py
@@ -0,0 +1,17 @@
+        
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+        s = sorted(arr)
+        if arr == s:
+            return []
+
+        flips = []
+        for pancake in range(n):
+            m = arr.index(max(arr[:n-pancake]))
+            if m != n-pancake-1:
+                arr[:m+1] = arr[:m+1][::-1]
+                arr[:n-pancake] = arr[:n-pancake][::-1]
+                flips.extend([m+1, n-pancake])
+        
+        return flips
