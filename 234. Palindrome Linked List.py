bb3eaedc118d45848504707a6830768301db429d
Melat t <melattesfaye16@gmail.com>
2024-09-04 02:55:17 +0300

    234. Palindrome Linked List

---
+++ 234. Palindrome Linked List.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from collections import deque
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        deq = deque()
+
+        while head:
+            deq.append(head.val)
+            head = head.next
+        
+        if len(deq) == 1:
+            return True
+
+        while len(deq) > 1:
+            if deq.popleft() != deq.pop():
+                return False
+        
+        return True
+
+        
